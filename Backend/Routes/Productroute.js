9aaa3cc8488bae3776af22cd36b4df1b4c684153
skyyy <aakashprajapati2457@gmail.com>
2025-12-20 14:41:07 +0530

    changed

--- Backend/Routes/Productroute.js
+++ Backend/Routes/Productroute.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import upload from '../Middlewares/multer.js';
 import {addproducts , listproduct , removeproduct, singleproduct , addReviews} from '../Controllers/ProductHandler.js'
-import { AdminAUth } from '../Middlewares/AdminAUth.js';
+import  AdminAUth  from '../Middlewares/AdminAuth.js';
  export  const Productrouter = express.Router();
 Productrouter.post('/add' , AdminAUth ,upload.fields([{name:'image1' , maxCount:1},{name:'image2' , maxCount:1},{name:'image3' , maxCount:1},{name:'image4' , maxCount:1}]),addproducts);
 Productrouter.post('/single' , singleproduct);
