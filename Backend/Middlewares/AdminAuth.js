ec183525417c63d1f94b9a21a7740ac8ae8ef010
skyyy <aakashprajapati2457@gmail.com>
2025-12-20 14:38:20 +0530

    final

--- Backend/Middlewares/AdminAuth.js
+++ Backend/Middlewares/AdminAuth.js
@@ -1,14 +1,11 @@
 import jwt from "jsonwebtoken";
 
-  export const AdminAuth = (req, res, next) => {
+const AdminAuth = (req, res, next) => {
   try {
     const { token } = req.headers;
 
     if (!token) {
-      return res.json({
-        success: false,
-        message: "Token not provided",
-      });
+      return res.json({ success: false, message: "Token not provided" });
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -17,20 +14,13 @@ import jwt from "jsonwebtoken";
       decoded !==
       process.env.ADMIN_USERNAME + process.env.ADMIN_PASSWORD
     ) {
-      return res.json({
-        success: false,
-        message: "You are not admin",
-      });
+      return res.json({ success: false, message: "You are not admin" });
     }
 
     next();
   } catch (error) {
-    return res.json({
-      success: false,
-      message: error.message,
-    });
+    return res.json({ success: false, message: error.message });
   }
 };
 
-
- 
+export default AdminAuth;
--- Backend/Routes/Orderroutes.js
+++ Backend/Routes/Orderroutes.js
@@ -1,6 +1,6 @@
 import { placeorder , placeorderwithrazorpay , placeorderwithstripe , allorder, updatestatus ,  userOders , verifystripe , verifyrazorpay } from "../Controllers/OrderController.js";
 import express from 'express'
-import { AdminAuth } from "../Middlewares/AdminAuth.js";
+import  AdminAuth  from "../Middlewares/AdminAuth.js";
 import Auth from "../Middlewares/Auth.js";
 const OrderRouter = express.Router();
 //all these two will be for the admin planel to update the status and and to get all the information about the order and changes in the orders
